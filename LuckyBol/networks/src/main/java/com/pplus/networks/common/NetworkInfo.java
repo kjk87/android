6881884ab7902e4949c98076ef4e1c97744fb360
kim <>
2025-04-17 23:04:36 +0900

    .

---
+++ LuckyBol/networks/src/main/java/com/pplus/networks/common/NetworkInfo.java
@@ -0,0 +1,16 @@
+package com.pplus.networks.common;
+
+/**
+ * Created by j2n on 2016. 9. 7..
+ */
+public interface NetworkInfo{
+
+    NetworkConfig.SWITCH isDebug();
+
+    NetworkConfig.DEVELOP_MODE getServiceMode();
+
+    String BaseRealServerUrl();
+
+    String BaseDevServerUrl();
+
+}
